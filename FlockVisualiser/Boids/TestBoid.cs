41dbd9a124f25c812be0555bdf4d4c563f554ed9
pearsonben <pearsonben98@gmail.com>
2022-01-28 14:50:55 +0000

    Added boids to GameBoardVM

---
+++ FlockVisualiser/Boids/TestBoid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlockVisualiser.Boids
+{
+    public class TestBoid
+    {
+
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+
+        public string ImageSource { get { return "/AssemblyName;component/Boids/Icons/Boid.png"; }  }
+
+        public double Angle { get; set; }
+
+    }
+}
--- FlockVisualiser/ViewModels/GameBoardViewModel.cs
+++ FlockVisualiser/ViewModels/GameBoardViewModel.cs
@@ -1,22 +1,40 @@
 ﻿using Caliburn.Micro;
+using FlockVisualiser.Boids;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Threading;
 namespace FlockVisualiser.ViewModels
 {
     public class GameBoardViewModel : Screen
     {
 
+        public string TestText { get; set; } = "GameBoarDViewModel";
+
+        public BindableCollection<TestBoid> GameEntities { get; set; } = new BindableCollection<TestBoid>()
+            {
+                new TestBoid{ Angle = 45, XPos = 100, YPos = 100 },
+                new TestBoid{ Angle = 90, XPos = 400, YPos = 300 }
+            };
+
+        //public DispatcherTimer GameTimer { get; set; } = new DispatcherTimer();
+
         public GameBoardViewModel()
         {
 
+
+            //GameEntities
+
+            //GameTimer.Tick += GameEngine;
+
+
         }
 
-
-
-
+        private void GameEngine(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
